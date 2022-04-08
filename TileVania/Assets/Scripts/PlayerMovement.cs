f8c11523a26cff6c1cbd96fcb0e073ec0d49e079
Mostafa Ayman <mostafa.aymanae@gmail.com>
2022-04-08 15:28:31 +0200

    horizontal player movement

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,34 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    
+    Rigidbody2D myRigidbody;
 
+    [SerializeField]
+    float playerSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        myRigidbody = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x*playerSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
     }
 }
