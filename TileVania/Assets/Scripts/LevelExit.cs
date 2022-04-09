e87c76320e2af7d7b8f1abf05d3deb5189407843
Mostafa Ayman <mostafa.aymanae@gmail.com>
2022-04-09 11:54:48 +0200

    load next level implementation

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    float levelLoadDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine(loadNextLevel(levelLoadDelay));
+        }
+    }
+
+
+    IEnumerator loadNextLevel(float levelLoadDelay)
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+}
